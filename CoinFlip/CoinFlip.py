3b8c699253b483d39541c99a34197fc65b34302f
Roger Pease <rogerpease@gmail.com>
2019-07-24 05:30:11 -0600

    Added flipping of coin button sequenes.

--- CoinFlip/CoinFlip.py
+++ CoinFlip/CoinFlip.py
@@ -1,14 +1,19 @@
 import pygame; 
+import sys; 
 
 (width, height) = (400, 300)
 screen = pygame.display.set_mode((width, height))
-WHITE=(255,255,255)
-BLUE=(0,0,255)
-YELLOW = (255,255,32)
+
+WHITE  = (255,255,255)
+BLUE   = (  0,  0,255)
+YELLOW = (255,255, 32)
+SILVER = (192,192,192)
+
 screen.fill(WHITE)
 
 flipRectangle = pygame.draw.rect(screen,YELLOW,(200, 50,100,50)) 
 quitRectangle = pygame.draw.rect(screen,YELLOW,(200,150,100,50))
+coinCircle    = pygame.draw.circle(screen,SILVER,(100,150),50)
 
 pygame.font.init() # you have to call this at the start, 
                    # if you want to use this module.
@@ -19,7 +24,9 @@ screen.blit(flipTextSurface,(210,60))
 quitTextSurface = myfont.render("Quit", False, (255,0,0))
 screen.blit(quitTextSurface,(210,160))
 coinTextSurface = myfont.render("Heads", False, (255,0,0))
-screen.blit(coinTextSurface,(100,150))
+
+coinTextCorner = (78,138)
+screen.blit(coinTextSurface,coinTextCorner)
 
 
 pygame.display.flip()
@@ -27,19 +34,19 @@ while True:
     for evnt in pygame.event.get():
       if evnt.type == pygame.MOUSEBUTTONDOWN:
         pos = pygame.mouse.get_pos()
-        print (pos) 
+        print (pos)
+        if flipRectangle.collidepoint(pos):
+          coinTextSurface = myfont.render("Tails", False,(255,0,0))
+          coinCircle    = pygame.draw.circle(screen,SILVER,(100,150),50)
+          screen.blit(coinTextSurface,coinTextCorner)
+        if quitRectangle.collidepoint(pos):
+          print ("Quit") 
+          sys.exit()
+
     pygame.display.flip()
     pass
 
-
-
-
-
-
-#coinCircle = pygame.draw.circle(screen,YELLOW,(200,50),40)
-#       if (pos[0] > 200): 
-#         print ("Right side")
-#	else 
-#         print ("Left side")
-#        if quitRectangle.collidepoint(pos):
-#          print ("Quit") 
+# TODO: Add Tails/Heads
+# TODO: Animate Tails/Heads or pull picture from internet of quarter. 
+# 23 Jul 2019: Added Coin body and over-drew coin on reflip. 
+   
